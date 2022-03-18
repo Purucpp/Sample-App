db09a2c5b96a1f16ac36934a3ddabb7f4351c335
purushottam kumar <purushottam.kumar@janitri.in>
2022-03-19 01:34:09 +0530

    phase 2 complete

--- app/src/main/java/in/yesandroid/sampleapp/MainActivity.kt
+++ app/src/main/java/in/yesandroid/sampleapp/MainActivity.kt
@@ -3,6 +3,9 @@ package `in`.yesandroid.sampleapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,12 +15,15 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    val data = ArrayList<ItemsViewModel>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
-
-
-
 
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
@@ -25,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         recyclerview.layoutManager = LinearLayoutManager(this)
 
         // ArrayList of class ItemsViewModel
-        val data = ArrayList<ItemsViewModel>()
 
         // This loop will create 20 Views containing
         // the image with the count of view
@@ -70,4 +75,61 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.example_menu, menu)
+        val searchitem = menu.findItem(R.id.action_search)
+        val searchView = searchitem.actionView as SearchView
+        //SearchView
+        searchView.queryHint = "Search content"
+
+        /*
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setQueryHint("Search Content"); */searchView.setOnSearchClickListener { }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                //mRecyclerAdapterApps.getFilter().filter(newText);
+                filter(newText.toLowerCase())
+                return false
+            }
+        })
+        return true
+    }
+
+    fun filter(text: String?) {
+       // val temp: MutableList<AppInfo?> = java.util.ArrayList<Any?>()
+        val temp = ArrayList<ItemsViewModel>()
+        for (d in data) {
+            //or use .equal(text) with you want equal match
+            //use .toLowerCase() for better matches
+            if (d.text.toLowerCase().contains(text.toString())) {
+                temp.add(d)
+            }
+        }
+
+        val updatedAdapter = CustomAdapter(temp)
+
+        val recyclervie = findViewById<RecyclerView>(R.id.recyclerview)
+
+        recyclervie.adapter=updatedAdapter
+
+        // Setting the Adapter with the recyclerview
+
+
+        //update recyclerview
+
+    }
+
 }
