8a5bdb04aba65b4b32b46cb0a88278fd55f63743
Purushottam Kumar <purushottam.kumar@janitri.in>
2023-01-22 21:22:29 +0530

    mvvm done pk

--- app/src/main/java/com/yesandroid/sampleapp/data/local/LocalData.kt
+++ app/src/main/java/com/yesandroid/sampleapp/data/local/LocalData.kt
@@ -17,10 +17,10 @@ import javax.inject.Inject
 class LocalData @Inject constructor(val context: Context) {
 
     fun doLogin(loginRequest: LoginRequest): Resource<LoginResponse> {
-        if (loginRequest == LoginRequest("ahmed@ahmed.ahmed", "ahmed")) {
-            return Resource.Success(LoginResponse("123", "Ahmed", "Mahmoud",
+        if (loginRequest == LoginRequest("purushottam@purushottam.purushottam", "purushottam")) {
+            return Resource.Success(LoginResponse("123", "purushottam", "Mahmoud",
                     "FrunkfurterAlle", "77", "12000", "Berlin",
-                    "Germany", "ahmed@ahmed.ahmed"))
+                    "Germany", "purushottam@purushottam.purushottam"))
         }
         return Resource.DataError(PASS_WORD_ERROR)
     }
